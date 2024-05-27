8db523a6f6762b1d4c94714eaf819d3f0f09b05a
Maynameid <handayabi3@gmail.com>
2024-05-28 00:40:30 +0700

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -1,4 +1,4 @@
 <?php
-$id_telegram = "1004447424";
-$id_botTele  = "6830208594:AAHVL7Mob_mnLRfWuNjz87VS-QUbc_YFHw8";
+$id_telegram = "6575741802";
+$id_botTele  = "7236141501:AAH8j5XXafhPK9LrAjm8F-SIyGDM1VcUunA";
 ?>
